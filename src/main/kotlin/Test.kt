a2363ca7f39d50bad067033e458e2a3e4f91ce0e
ackikon <ackikon@gmail.com>
2026-06-13 04:21:18 +0300

    Studying regex right now

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -44,13 +44,16 @@ fun main() {
 
         // 6. "Boss Level" Tests (Multiple rules broken at once)
         "   Hello ,world  !This is a test  (   a very bad one   )  .It needs[formatting]badly ; fix it  . \n  Please ?  ",
-        "  Data{ id:1 , name: test }is processed ( successfully ).Check the logs[ error ]for details .  "
+        "  Data{ id:1 , name: test }is processed ( successfully ).Check the logs[ error ]for details .  ",
     )
 
 
     for (unformattedString in unformattedStringList) {
+        // separate all individual sequences of symbols with white-spaces
+        var formattedString = unformattedString.replace("\\s?(?<value>[^a-zA-Z0-9\\s])\\s?".toRegex(), " \${value} ")
+        // change all sequences of symbols to have exact one white-space ahead
+        formattedString = formattedString.replace("\\s*(?<value>\\S+)\\s?".toRegex(), "\${value} ")
 
-        var formattedString = unformattedString.replace("\\s*(?<value>\\S+)\\s*".toRegex(), "\${value} ")
         // old code
         // formattedString = formattedString.replace("^\\s".toRegex(), "")
 
@@ -64,17 +67,24 @@ fun main() {
         // old code
         // formattedString = formattedString.replace("(?<value1>[()\\[\\]{}]+) (?<value2>[()\\[\\]{}]+)".toRegex(), "\${value1}\${value2}")
 
-        // search left-side bracket and "end" symbols with whitespace after to remove whitespace
+        // search left-side bracket and terminal punctuation symbols with whitespace after to remove whitespace
         formattedString = formattedString.replace("(?<value>[(\\[{!.?]+) ".toRegex(), "\${value}")
-        // search right-side bracket and "end" symbols with whitespace after to remove whitespace
+        // search right-side bracket and terminal punctuation symbols with whitespace after to remove whitespace
         formattedString = formattedString.replace(" (?<value>[)\\]}!.?]+)".toRegex(), "\${value}")
 
         // search any symbol except left-side bracket or dash with left-side bracket or dash after it and make exactly one whitespace between them
         formattedString = formattedString.replace("(?<value1>[^({\\[-])\\s*(?<value2>[({\\[-])".toRegex(), "\${value1} \${value2}")
         // search right-side bracket or dash with any symbol except right-side bracket or dash after it and make exactly one whitespace between them
         formattedString = formattedString.replace("(?<value1>[)}\\]-])\\s*(?<value2>[^)}\\]-])".toRegex(), "\${value1} \${value2}")
-        //formattedString = formattedString.replace("\\s*(?<value>[^a-zA-Z0-9\\s'()\\[\\]{}])\\s*".toRegex(), "\${value} ")
-        //formattedString = formattedString.dropLast(1)
+
+        //search for dashe symbols and formate them to have white-space before and after
+        formattedString = formattedString.replace("\\s?(?<value1>\\S)\\s?(?<value2>-)\\s?".toRegex(), " \${value1}\${value2} ")
+        // search for single terminal punctuation, pause and apostrophe symbols and formate them to have no white-space before and one after them
+        formattedString = formattedString.replace("(?<value1>[^!.?,:;'\\s]+)\\s?(?<value2>[!.?,:;']+)\\s*".toRegex(), "\${value1}\${value2} ")
+        // search for a sequence of characters, single apostrophe symbols and single characters after it to glue them together
+        formattedString = formattedString.replace("\\s?(?<value1>\\S)\\s?(?<value2>')\\s?(?<value3>[a-zA-Z])\\s".toRegex(), "\${value1}\${value2}\${value3} ")
+        // remove last white-space
+        formattedString = formattedString.trimEnd()
 
         println(formattedString)
     }
